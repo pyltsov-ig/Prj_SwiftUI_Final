4f6416b374880d112c0ea11b5ffc79bd575bd9f6
IGP <pyltsov@gmail.com>
2021-08-07 10:03:00 +0300

    Initial Commit

---
+++ Prj_SwiftUI_Final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prj_SwiftUI_Final
+//
+//  Created by Igor Pyltsov on 07.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prj_SwiftUI_Final/Prj_SwiftUI_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prj_SwiftUI_FinalApp.swift
+//  Prj_SwiftUI_Final
+//
+//  Created by Igor Pyltsov on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Prj_SwiftUI_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
